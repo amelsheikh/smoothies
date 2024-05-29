465c94b164cb742ff6bc0e8fdb32af7b4419d2f5
AbdAllah Elsheikh <aelsheikh135@gmail.com>
2024-05-29 10:42:21 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,8 +32,8 @@ if ingredients_list:
     ingredients_string = ''
     for fruit_item in ingredients_list:
         ingredients_string += fruit_item+","
-        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-        #st.text(fruityvice_response.json())
+        st.subheader(fruit_item + ' Nutrition Information')
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_item)
         fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
     
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
